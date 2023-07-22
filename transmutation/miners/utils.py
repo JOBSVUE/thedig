55682806c13e98159f9846811f803c1f4e7583e5
Badreddine <badreddine@ankaboot.fr>
2023-07-22 10:29:20 +0200

    moved stuff to utils

---
+++ transmutation/miners/utils.py
@@ -0,0 +1,19 @@
+"""
+Various utilities
+"""
+__author__ = "Badreddine LEJMI <badreddine@ankaboot.fr>"
+__license__ = "AGPL"
+
+from thefuzz import fuzz
+
+TOKEN_RATIO = 96
+
+def match_name(name: str, text: str) -> bool:
+    if not name:
+        return True
+    return fuzz.partial_token_sort_ratio(name, text) >= TOKEN_RATIO
+
+def normalize(name: str) -> str:
+    return name.encode(
+        "ASCII", "ignore"
+        ).strip().lower().decode().replace(' ', '')

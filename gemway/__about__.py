b8cf5e32f5f50e20ab69b77a9a0f67af5da0f67b
Badreddine <badreddine@ankaboot.fr>
2023-08-15 17:16:26 +0200

    boilerplate

---
+++ gemway/__about__.py
@@ -0,0 +1,7 @@
+# Copyright 2023 Badreddine LEJMI.
+# SPDX-License-Identifier: 	AGPL-3.0-or-later
+
+__author__ = "Badreddine LEJMI <badreddine@ankaboot.fr>"
+__copyright__ = "Ankaboot"
+__license__ = "AGPL"
+__version__ = "0.1"

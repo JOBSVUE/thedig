7a02ea167b159ec2a69578803ede6764eeacba65
Badreddine <badreddine@ankaboot.fr>
2023-08-15 17:30:59 +0200

    license made dynamic

--- gemway/__about__.py
+++ gemway/__about__.py
@@ -3,5 +3,5 @@
 
 __author__ = "Badreddine LEJMI <badreddine@ankaboot.fr>"
 __copyright__ = "Ankaboot"
-__license__ = "AGPL"
+__license__ = "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)"
 __version__ = "0.1"

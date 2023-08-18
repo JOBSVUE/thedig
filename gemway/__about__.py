d5f37c67de82bc47295037301fbc9469b5c4f668
Badreddine <badreddine@ankaboot.fr>
2023-08-18 14:35:15 +0200

    fix order of variables

--- gemway/__about__.py
+++ gemway/__about__.py
@@ -1,8 +1,8 @@
 # Copyright 2023 Badreddine LEJMI.
 # SPDX-License-Identifier: 	AGPL-3.0-or-later
+__email__ = "badreddine@ankaboot.fr"
 __author__ = f"Badreddine LEJMI <{__email__}>"
 __copyright__ = "Ankaboot"
-__email__ = "badreddine@ankaboot.fr"
 __license__ = "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)"
 __summary__ = "Microservices API for data enrichment using determinist, IA and legit OSINT techniques on your contacts"
 __title__ = "Gemway API"
